000b5a10d3f1638daf002b7bbdc954737129fa45
Ryder Stanley <ryderstanley2007@gmail.com>
2020-04-19 18:10:24 -0600

    formatting

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main {
 	static Scanner sc = new Scanner(System.in);
 
 	static int[][] moves = { {-2, +1},
-						   {-1, +2},
-						   {+1, +2},
-						   {+2, +1},
-						   {+2, -1},
-						   {+1, -2},
-						   {-1, -2},
-						   {-2, -1} };
+			       {-1, +2},
+			       {+1, +2},
+			       {+2, +1},
+			       {+2, -1},
+			       {+1, -2},
+			       {-1, -2},
+			       {-2, -1} };
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to the "
